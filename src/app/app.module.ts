134621f4b96bc72b6eadc0d39f697ea6a5301d35
Algirdas Saliamonas <algirdas.saliamonas@insoft.lt>
2020-12-02 03:00:01 +0200

    added front logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FileUploadModule} from '@iplab/ngx-file-upload';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { UploadFilesComponent } from './components/upload-files/upload-files.component';
 
 @NgModule({
@@ -12,6 +15,9 @@ import { UploadFilesComponent } from './components/upload-files/upload-files.com
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    FileUploadModule,
     AppRoutingModule
   ],
   providers: [],
